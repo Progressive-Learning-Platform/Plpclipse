54febb51ba26e3540ef351865e479109c1490f9f
hariiyerasu1729 <hari.iyer@asu.edu>
2016-10-30 17:25:41 -0700

    Delete SnapshotEventHandler.java

--- PluginSim1/PluginSim1/src/plptool/mips/SnapshotEventHandler.java
+++
@@ -1,8 +0,0 @@
-package plptool;
-
-import org.json.JSONException;
-
-public abstract class SnapshotEventHandler implements SnapshotListener {
-	@Override
-	public void receiveSnapshot(String color) throws JSONException {};
-}

531a79a42796af70e1d339db2ba0686ac0618c84
hariiyerasu1729 <hari.iyer@asu.edu>
2016-11-27 20:08:55 -0700

    Update ValuePopupMenu.java
    Comment updated.

--- PluginSim1/PluginSim1/src/plptool/visualizer/graphs/ValuePopupMenu.java
+++ PluginSim1/PluginSim1/src/plptool/visualizer/graphs/ValuePopupMenu.java
@@ -9,21 +9,17 @@ import com.mxgraph.model.mxCell;
 
 public class ValuePopupMenu extends JPopupMenu {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -8439317231714112220L;
-
+	
+	/**
+	 * Constructor, used for populating the PopupMenu with the component values
+	 */
 	public ValuePopupMenu(mxCell cell) {
 		String []values = cell.getValue().toString().split(",");
 		for (String value: values) {
 			AbstractAction newAction = new AbstractAction(value)
 			{
-				/**
-				 * 
-				 */
 				private static final long serialVersionUID = 3604632160824380941L;
-
 				public void actionPerformed(ActionEvent e)
 				{
 				}
